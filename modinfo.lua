e682ac990abdfab3dcfa1a5c7b4a42b92ce70810
tetrisface <89279737+tetrisface@users.noreply.github.com>
2025-09-24 01:17:16 +0200

    Update modinfo.lua

--- modinfo.lua
+++ modinfo.lua
@@ -6,8 +6,8 @@ return {
     mutator='0', -- or '1' if it's a mutator
     game='Beyond All Reason',
     shortname='modtest3',
-    mutator='1',
-    modtype=3,           -- Mutator type
+    mutator=1,
+    modtype=1,           -- Mutator type
     depend = {
         [[rapid://byar:stable]],
     }
