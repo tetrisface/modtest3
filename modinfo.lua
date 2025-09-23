939208c73169db42cb17c7b6a32db763a3ca4d1f
tetrisface <89279737+tetrisface@users.noreply.github.com>
2025-09-24 01:17:31 +0200

    Update modinfo.lua

--- modinfo.lua
+++ modinfo.lua
@@ -3,7 +3,6 @@ return {
     description='Your mod description',
     version='v1.0.0',
     shortname='YOURMOD',
-    mutator='0', -- or '1' if it's a mutator
     game='Beyond All Reason',
     shortname='modtest3',
     mutator=1,
