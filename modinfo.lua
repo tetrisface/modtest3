027e0d40ce0fabbeed8d99b53e9bb0ba63942e48
tetrisface <89279737+tetrisface@users.noreply.github.com>
2025-09-24 21:04:41 +0200

    Update modinfo.lua

--- modinfo.lua
+++ modinfo.lua
@@ -1,12 +1,12 @@
 return {
     name='mod test 3',
-    description='Your mod description',
+    description='Test mod with UI widget',
     version='v1.0.0',
-    shortname='YOURMOD',
-    game='Beyond All Reason',
     shortname='modtest3',
-    mutator=1,
-    modtype=1,           -- Mutator type
+    mutator='1',
+    game='Beyond All Reason',
+    shortGame='BAR',
+    modtype=3,
     depend = {
         [[rapid://byar:stable]],
     }
